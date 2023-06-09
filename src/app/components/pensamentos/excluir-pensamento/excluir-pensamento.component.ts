4c1e356a2920c9fcdcbc4f31e53f1d9ae4aa80a5
Halysson Freitas <halyssonfreitas@gmail.com>
2023-06-09 14:31:35 -0300

    :sparkles: add component

---
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { IPensamento } from '../Ipensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  pensamento: IPensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe(pensamento => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento() {
+    if (! this.pensamento.id)
+      return
+    this.service.excluir(this.pensamento.id).subscribe(()=>{
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
+}
