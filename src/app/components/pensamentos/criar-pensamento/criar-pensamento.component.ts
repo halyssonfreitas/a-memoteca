46553be96b8df3bfa849ae83b8bbf5ad9ea8a007
Halysson Freitas <halyssonfreitas@gmail.com>
2023-06-09 14:30:24 -0300

    :sparkles: add criar pensamento

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { IPensamento } from '../Ipensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: IPensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    alert("Ação cancelada!")
+  }
+
+}
