945bd023ada6b2b7eef2b00a0d8d685f40ad2b17
Halysson Freitas <halyssonfreitas@gmail.com>
2023-06-09 17:55:38 -0300

    :hammer: reactive form and validations for criar pensamento

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IPensamento } from '../Ipensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,28 +11,49 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento: IPensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
+
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/(.|\s)*\S(.|\s)/)
+        ])
+      ],
+      autoria: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3)
+        ])
+      ],
+      modelo: ['modelo1']
+    })
   }
 
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
+    console.log(this.formulario.status)
+    if(!this.formulario.valid)
+      return
+    this.service.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento'])
     })
   }
 
   cancelar() {
     alert("Ação cancelada!")
+  }
+
+  habilitarBotao(): string {
+    return this.formulario.valid ?
+      'botao' : 'botao__desabilitado'
   }
 
 }
